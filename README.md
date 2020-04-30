f61c2ca488bf5ac113e1f6690478978335c8c280
James Jensen <james.jensen@recursionpharma.com>
2020-04-30 17:04:28 -0600

    add docstrings; move sample_weights to fit(); mention supported models

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 Python wrapper around glmnet R package for generalized linear models,
 providing scikit-learn style estimators.
 
+Supports linear, logistic, and multinomial (multi-class) models.
+
 To install dependencies:
 ```
 conda install numpy pandas scikit-learn rpy2
--- glmnet.py
+++ glmnet.py
@@ -17,7 +17,9 @@ stats = importr('stats')
 as_null = ro.r['as.null']
 
 """
-TODO: 
+TODO:
+- Add more detail about parameters in docstrings
+  - list what options are supported for each model, such as losses
 - use intercept and coefficients to predict etc. without invoking R?
    May allow use of a pre-fit model on a machine where rpy2 is not set up.
    Though I'm not sure because the self.model attribute is an R object.
@@ -62,6 +64,9 @@ def with_numpy2ri(func):
 
 
 class GLMNetCV(BaseEstimator):
+    """
+    Base class for all wrapped models.
+    """
 
     def __init__(self,
                  lower=None,
@@ -80,10 +85,26 @@ class GLMNetCV(BaseEstimator):
         self.lambda_min = None
         self.loss = None
         self.reg_type = None
-        self.sample_weight = None
         self.class_weight = None
 
-    def fit(self, x, y):
+    def fit(self, x, y, sample_weight=None):
+        """
+        Fit generalized linear model.
+
+        Parameters
+        ----------
+        x: array-like of shape (n_samples, n_features)
+            Training data
+        y: array-like of shape (n_samples,)
+            Target values
+        sample_weight: array-like of shape (n_samples,), default=None
+            Individual weights for each sample
+
+        Returns
+        -------
+        self: GLMNetCV object
+            Fitted estimator
+        """
         x_used = x.values if isinstance(x, pd.DataFrame) else np.array(x)
         y_used = y.values if isinstance(y, (pd.DataFrame, pd.Series)) else np.array(y)
         self.model = glmnet_fit(x_used,
@@ -94,11 +115,11 @@ class GLMNetCV(BaseEstimator):
                                 loss_metric=self.loss_metric,
                                 alpha=self.alpha,
                                 class_weight=self.class_weight,
-                                sample_weight=self.sample_weight)
+                                sample_weight=sample_weight)
 
         self.lambda_min = np.array(dollar(self.model, 'lambda.min'))[0]
         self.intercept_, self.coef_ = get_coeffs(self.model, lmda=self.lambda_min)
-        cv_losses = get_values_from_glmnet_fit(self.model, 'cvm')
+        cv_losses = get_attr_from_glmnet_fit(self.model, 'cvm')
         self.loss = np.min(cv_losses)
         return self
 
@@ -106,16 +127,35 @@ class GLMNetCV(BaseEstimator):
     def _pred(self,
               x,
               pred_type=None):
+        """
+        Base function for prediction
+        
+        Parameters
+        ----------
+        x: array-like of shape (n_samples, n_features)
+            Data to predict from.
+        pred_type: {'class', 'response', None}
+            glmnet param for whether to predict class, probability, or value.
+
+        Returns
+        -------
+        raw_pred:
+            Raw prediction results
+        """
         x_used = x.values if isinstance(x, pd.DataFrame) else np.array(x)
         if pred_type is None:
             pred_type = as_null()
-        return ro.r['predict'](self.model,
-                               newx=x_used,
-                               s=self.lambda_min,
-                               type=pred_type)
+        raw_pred = ro.r['predict'](self.model,
+                                   newx=x_used,
+                                   s=self.lambda_min,
+                                   type=pred_type)
+        return raw_pred
 
 
 class GLMNetClassifierCV(GLMNetCV, ClassifierMixin):
+    """
+    Base class for classifier models.
+    """
 
     def __init__(self,
                  class_weight=None,
@@ -125,16 +165,48 @@ class GLMNetClassifierCV(GLMNetCV, ClassifierMixin):
         self.class_weight = class_weight
 
     def predict(self, x):
+        """
+        Predict class membership for each sample.
+        
+        
+        Parameters
+        ----------
+        x: array-like of shape (n_samples, n_features)
+            Data to predict from.
+
+        Returns
+        -------
+        pred : array-like
+            Predicted probabilities of class membership for each sample.
+
+        """
         raw_pred = self._pred(x, pred_type='class')
         pred = np.array(list(map(int, raw_pred)))
         return pred
 
     def predict_proba(self, x):
-        raw_pred = self._pred(x, pred_type='response')
-        return np.array(raw_pred).T[0]
+        """
+        Predict probabilities of class membership for each sample.
+        
+        Parameters
+        ----------
+        x: array-like of shape (n_samples, n_features)
+            Data to predict from.
+
+        Returns
+        -------
+        pred_proba : array-like
+            Predicted probabilities of class membership for each sample.
+        """
+        raw_pred_proba = self._pred(x, pred_type='response')
+        pred_proba = np.array(raw_pred_proba).T[0]
+        return pred_proba
 
 
 class GLMNetRegressionCV(GLMNetCV, RegressorMixin):
+    """
+    Base class for regression models.
+    """
     def __init__(self,
                  sample_weight=None,
                  **kwargs):
@@ -143,28 +215,79 @@ class GLMNetRegressionCV(GLMNetCV, RegressorMixin):
         self.sample_weight = sample_weight
 
     def predict(self, x):
+        """
+        Predict values for each sample.
+        
+        Parameters
+        ----------
+        x: array-like of shape (n_samples, n_features)
+            Data to predict from.
+
+        Returns
+        -------
+        pred : array-like
+            Predicted values for each sample
+        """
         raw_pred = self._pred(x)
         pred = np.array(raw_pred).T[0]
         return pred
 
 
 class GLMNetLinearRegressionCV(GLMNetRegressionCV):
+    """
+    Regularized linear regression tuned by cross-validation.
+    
+    Parameters
+    ----------
+    lower: float, default=None
+        Lower bound on coefficients. E.g. lower=0 is non-negative regression
+    upper: float, default=None
+        Upper bound on coefficients
+    loss_metric: str, default='mse'
+        Which loss metric to minimize.
+    alpha: float, default=0.5
+        Balance of L1 and L2 regularization
+
+    See Also
+    --------
+    https://web.stanford.edu/~hastie/glmnet/glmnet_alpha.html#lin
+    """
 
     def __init__(self,
                  lower=None,
                  upper=None,
                  loss_metric='mse',
-                 alpha=0.5,
-                 sample_weight=None):
+                 alpha=0.5):
         super().__init__(lower=lower,
                          upper=upper,
                          loss_metric=loss_metric,
-                         alpha=alpha,
-                         sample_weight=sample_weight)
+                         alpha=alpha)
         self.reg_type = 'linear'
 
 
 class GLMNetLogisticRegressionCV(GLMNetClassifierCV):
+    """
+    Regularized logistic regression tuned by cross-validation.
+    
+    For binary classification tasks only.
+    
+    Parameters
+    ----------
+    lower: float, default=None
+        Lower bound on coefficients. E.g. lower=0 is non-negative regression
+    upper: float, default=None
+        Upper bound on coefficients
+    loss_metric: str, default='mse'
+        Which loss metric to minimize.
+    alpha: float, default=0.5
+        Balance of L1 and L2 regularization
+    class_weight: dict or ‘balanced’, default=None
+        Weights associated with classes. As in sklearn.
+
+    See Also
+    --------
+    https://web.stanford.edu/~hastie/glmnet/glmnet_alpha.html#log
+    """
     def __init__(self,
                  lower=None,
                  upper=None,
@@ -180,6 +303,28 @@ class GLMNetLogisticRegressionCV(GLMNetClassifierCV):
 
 
 class GLMNetMultinomialRegressionCV(GLMNetClassifierCV):
+    """
+    Regularized multinomial regression tuned by cross-validation.
+    
+    For multi-class classification tasks.
+    
+    Parameters
+    ----------
+    lower: float, default=None
+        Lower bound on coefficients. E.g. lower=0 is non-negative regression
+    upper: float, default=None
+        Upper bound on coefficients
+    loss_metric: str, default='mse'
+        Which loss metric to minimize.
+    alpha: float, default=0.5
+        Balance of L1 and L2 regularization
+    class_weight: dict or ‘balanced’, default=None
+        Weights associated with classes. As in sklearn.
+
+    See Also
+    --------
+    https://web.stanford.edu/~hastie/glmnet/glmnet_alpha.html#log
+    """
     def __init__(self,
                  lower=None,
                  upper=None,
@@ -195,12 +340,33 @@ class GLMNetMultinomialRegressionCV(GLMNetClassifierCV):
 
 
 def get_coeffs(cvfit, lmda='min'):
+    """
+    Retrieve coefficients, including intercept(s), from a fitted glmnet model.
+    
+    Parameters
+    ----------
+    cvfit: rpy2 GLMNet model object
+        Fit model
+    lmda: {'min', '1se', or float}, default='min'
+        Which regularization strength to retrieve coefficients for.
+        'min' gives the regularization with minimum loss.
+        '1se' gives the strongest regularization within 1 std. dev. of the min.
+        A float specifies the actual value of the regularization parameter. 
+
+    Returns
+    -------
+    intercept: array-like
+        Intercept(s) of the model
+    coeffs : array-like
+        Coefficients of the model
+
+    """
     if not isinstance(lmda, numbers.Number):
         if isinstance(lmda, str):
             if lmda not in ['min', '1se']:
                 raise ValueError(f"{lmda} not an accepted lmda; try 'min', '1se', or a number")
             else:
-                lmda = get_values_from_glmnet_fit(cvfit, f'lambda.{lmda}')[0]
+                lmda = get_attr_from_glmnet_fit(cvfit, f'lambda.{lmda}')[0]
         else:
             raise ValueError("lmda must be a string or number")
     r = ro.r
@@ -224,6 +390,40 @@ def glmnet_fit(x,
                alpha=0.5,
                class_weight=None,
                sample_weight=None):
+    """
+    Fit generalized linear model in glmnet, tuning with cross-validation
+
+    Parameters
+    ----------
+    x: array-like of shape (n_samples, n_features)
+        Training data
+    y: array-like of shape (n_samples,)
+        Target values
+    reg_type: {'linear', 'logistic', 'multinomial'}
+        Type of regression model.
+    lower: float, default=None
+        Lower bound on coefficients. E.g. lower=0 is non-negative regression
+    upper: float, default=None
+        Upper bound on coefficients
+    loss_metric: str
+        Which loss metric to minimize. Supported options and their meaning may
+        vary by model.
+    alpha: float, default=0.5
+        Balance of L1 and L2 regularization
+    class_weight: dict or ‘balanced’, default=None
+        Weights associated with classes (classification only). As in sklearn.
+    sample_weight: array-like of shape (n_samples,), default=None
+        Individual weights for each sample
+
+    Returns
+    -------
+    fit: rpy2 GLMNet model object
+        Fit model
+
+    See Also
+    --------
+    https://web.stanford.edu/~hastie/glmnet/glmnet_alpha.html#qs
+    """
     lower = float('-inf') if lower is None else lower
     upper = float('inf') if upper is None else upper
     if reg_type in ['logistic', 'multinomial']:
@@ -244,10 +444,26 @@ def glmnet_fit(x,
     return fit
 
 
-def get_values_from_glmnet_fit(fit, field):
+def get_attr_from_glmnet_fit(fit, attr):
+    """
+    Retrieve a particular attribute from a glmnet model object.
+    
+    Parameters
+    ----------
+    fit: rpy2 GLMNet model object
+        Fit model
+    attr: str
+        Attribute of model to retrieve
+
+    Returns
+    -------
+    values: array-like
+        Values of specified attribute
+    """
     names = list(fit.names)
-    if field not in names:
-        raise ValueError("{} not a field of glmnet fit object".format(field))
-    return np.array(fit[names.index(field)])
+    if attr not in names:
+        raise ValueError("{} not a field of glmnet fit object".format(attr))
+    values = np.array(fit[names.index(attr)])
+    return values
 
 
